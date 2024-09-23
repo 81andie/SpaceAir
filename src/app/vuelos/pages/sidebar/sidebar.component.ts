95018cbf7d484dc1d859888f1e567205b3704971
81andie <133395577+81andie@users.noreply.github.com>
2024-09-23 22:06:39 +0200

    cambios en el sidebar

--- src/app/vuelos/pages/sidebar/sidebar.component.ts
+++ src/app/vuelos/pages/sidebar/sidebar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
 
+
+  public isSidebarVisible:boolean = true;
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
 }
+
